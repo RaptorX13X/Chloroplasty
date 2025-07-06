a7dc373c23ed6b42bc4f56878a3aa6f054204430
Raptor <panraptor13@gmail.com>
2025-07-06 03:24:50 +0200

    drinks & ingredients

---
+++ Assets/Scripts/Drinks/DrinkSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IngredientSO", menuName = "IngredientSO")]
+public class Ingredient : ScriptableObject
+{
+    public string name;
+}
+
+[CreateAssetMenu(fileName = "DrinkSO", menuName = "DrinkSO")]
+public class DrinkSO : ScriptableObject
+{
+    public Ingredient[] ingredients;
+}
